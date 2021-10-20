0db568ce0ea69725fe49a92a659b04a77f979090
Hermawan Mulyono <hmulyono3@gatech.edu>
2021-10-21 00:52:29 +1100

    Clustering example

--- assignment3/main.py
+++ assignment3/main.py
@@ -1,5 +1,8 @@
+from sklearn.cluster import KMeans
+
 from utils.data import gen_3d_data
 from utils.plots import visualize_3d_data
+
 
 
 def dataset1():
@@ -13,8 +16,18 @@ def dataset1():
     x_train, y_train, x_val, y_val, x_test, y_test = gen_3d_data(
         x1_size, x2_size, n_train, n_val, n_test, noise_prob)
 
-    fig = visualize_3d_data(x_train, y_train, 'Dataset3D')
+    fig = visualize_3d_data(x_train, y_train, ['negative', 'positive'])
     fig.show()
+
+    for n_cluster in range(10, 15):
+        clusterer = KMeans(n_cluster)
+        cluster_labels = clusterer.fit_predict(x_train)
+
+        cluster_names = [f'cluster_{l}' for l in clusterer.labels_]
+        visualize_3d_data(x_train, cluster_labels, cluster_names).show()
+
+
+
 
 
 def main():
--- assignment3/utils/plots.py
+++ assignment3/utils/plots.py
@@ -1,16 +1,17 @@
+from typing import List
+
 import numpy as np
 from plotly import graph_objects as go
 
 
-def visualize_3d_data(x_data: np.ndarray,
-                      y_data: np.ndarray,
-                      title: str = None) -> go.Figure:
+def visualize_3d_data(x_data: np.ndarray, y_data: np.ndarray,
+                      categories: List[str]) -> go.Figure:
     """Visualizes 3D data
 
     Args:
         x_data: A (num_examples, 3) array.
         y_data: A (num_examples, ) array of labels corresponding to `x_data`.
-        title: If given, will add plot title
+        categories: Category names of the data, ordered by indices
 
     Returns:
         A plotly figure object
@@ -21,23 +22,16 @@ def visualize_3d_data(x_data: np.ndarray,
     fig = _add_scatter_dataset3d(fig,
                                  x_data,
                                  y_data,
-                                 scatter_alpha=0.2,
-                                 scatter_size=2)
-    fig.update_layout({
-        'xaxis_title': 'x1',
-        'yaxis_title': 'x2',
-        # 'width': 960,
-        # 'height': 540
-    })
-
-    if title is not None:
-        fig.update_layout({'title': title})
+                                 scatter_alpha=0.5,
+                                 scatter_size=4,
+                                 categories=categories)
+    fig.update_layout({'xaxis_title': 'x1', 'yaxis_title': 'x2'})
 
     return fig
 
 
 def _add_scatter_dataset3d(fig: go.Figure, x_data, y_data, scatter_alpha: float,
-                           scatter_size: int):
+                           scatter_size: int, categories: List[str]):
     """Adds Dataset2D scatter plot
 
     Args:
@@ -46,37 +40,31 @@ def _add_scatter_dataset3d(fig: go.Figure, x_data, y_data, scatter_alpha: float,
         y_data: Dataset2D labels
         scatter_alpha: Transparency
         scatter_size: Scatter size
+        categories: Category names of the data, ordered by indices
 
     Returns:
         A Figure object with scatter
 
+
     """
-    positive_indices = y_data == 1
+    possible_labels = sorted(set(y_data))
+    if not ((0 <= min(possible_labels)) and
+            (max(possible_labels) < len(categories))):
+        raise ValueError(
+            f'Possible labels are not consistent with the labels {categories}')
 
-    # Sanity check
-    assert np.all(y_data[np.logical_not(positive_indices)] == 0)
+    for label, category_name in zip(possible_labels, categories):
+        selected_indices = (y_data == label)
+        x_positive = x_data[selected_indices]
 
-    x_positive = x_data[positive_indices]
-    x_negative = x_data[np.logical_not(positive_indices)]
-
-    fig.add_trace(
-        go.Scatter3d(x=x_positive[:, 0],
-                     y=x_positive[:, 1],
-                     z=x_positive[:, 2],
-                     mode='markers',
-                     name='positive',
-                     marker={
-                         'color': f'rgba(255,0,0,{scatter_alpha})',
-                         'size': scatter_size
-                     }))
-    fig.add_trace(
-        go.Scatter3d(x=x_negative[:, 0],
-                     y=x_negative[:, 1],
-                     z=x_negative[:, 2],
-                     mode='markers',
-                     name='negative',
-                     marker={
-                         'color': f'rgba(0,0,255,{scatter_alpha})',
-                         'size': scatter_size
-                     }))
+        fig.add_trace(
+            go.Scatter3d(x=x_positive[:, 0],
+                         y=x_positive[:, 1],
+                         z=x_positive[:, 2],
+                         mode='markers',
+                         name=category_name,
+                         marker={
+                             'size': scatter_size,
+                             'opacity': 1 - scatter_alpha
+                         }))
     return fig
